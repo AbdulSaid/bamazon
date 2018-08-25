f293594c223229cbf82d4fcf739dd627289dcefb
Abdul Said <abdulrahman.asaid@gmail.com>
2018-08-25 16:55:42 -0400

    added inquirer prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,19 +14,47 @@ connection.connect(function(err) {
   console.log('connected as id ' + connection.threadId);
   showProducts();
   connection.end();
+  askUser();
 });
 
 function showProducts() {
   connection.query('SELECT * FROM products', function(err, res) {
     if (err) throw err;
-    console.log(res);
+    console.log('Items Avalable:');
+    for (var i = 0; i < res.length; i++) {
+      console.log('Item ID: ' + res[i].item_id);
+      console.log('Product Name: ' + res[i].product_name);
+      console.log('Price: ' + res[i].price);
+    }
   });
 }
+function validateItem(name) {
+  var reg = /^\d+$/;
+  return reg.test(name) || 'ID should be a number!';
+}
+function validateUnits(name) {
+  var reg = /^\d+$/;
+  return reg.test(name) || 'Units should be a number!';
+}
 
-// inquirer
-//   .prompt([
-
-//   ])
-//   .then(answers => {
-
-//   });
+function askUser() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'itemToBuy',
+        message:
+          'What item would you like to buy, please type in the ID number',
+        validate: validateItem
+      },
+      {
+        type: 'input',
+        name: 'howMuch',
+        message: 'How much units would you like?',
+        validate: validateUnits
+      }
+    ])
+    .then(function(answers) {
+      console.log('finished');
+    });
+}
