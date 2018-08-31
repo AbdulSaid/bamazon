b72c1fb66c53226f66a8de18b5de88d6543e96c6
Abdul Said <abdulrahman.asaid@gmail.com>
2018-08-30 20:55:41 -0400

    finalize manager

--- bamazonManager.js
+++ bamazonManager.js
@@ -49,7 +49,7 @@ function askManager() {
           break;
 
         case 'End Connection':
-          connection.end();
+          endConnection();
           break;
       }
     });
@@ -76,23 +76,128 @@ function showProducts() {
 function lowInventory() {
   connection.query('SELECT * FROM products', function(err, res) {
     if (err) throw err;
+    console.log('Items with Low Inventory');
     for (var i = 0; i < res.length; i++) {
       var data = res[i];
+
       if (data.stock_quantity < 5) {
-        console.log('Items with Low Inventory');
-        console.log('Item Id' + data.item_id);
-        console.log('Product Name:' + data.product_name);
+        console.log('----------------------------');
+        console.log('Item Id: ' + data.item_id);
+        console.log('Product Name: ' + data.product_name);
         console.log('Quantity: ' + data.stock_quantity);
-        console.log('Try adding more inventory');
-      } else {
-        //repeats multiple times
-        console.log('No items have low inventory');
+        console.log('----------------------------\n');
       }
     }
+    console.log('Try adding more inventory\n');
     askManager();
   });
 }
 
-function addInventory() {}
+function addInventory() {
+  inquirer
+    .prompt([
+      {
+        name: 'addInventory',
+        type: 'input',
+        message: 'What item would you like to add more, please list ID number?',
+        validate: validateItem
+      },
+      {
+        name: 'numberInventory',
+        type: 'input',
+        message: 'How much total units would you like to add?',
+        validate: validateUnits
+      }
+    ])
+    .then(function(answer) {
+      var itemtoUpdate = answer.addInventory;
+      var numbertoUpdate = answer.numberInventory;
 
-function addProduct() {}
+      connection.query(
+        'Update products Set ? WHERE ?',
+        [
+          {
+            stock_quantity: numbertoUpdate
+          },
+          {
+            item_id: itemtoUpdate
+          }
+        ],
+        function(err, data) {
+          if (err) throw err;
+          console.log('\nData Base Updated, new updated list below');
+        }
+      );
+      showProducts();
+      askManager();
+    });
+}
+
+function addProduct() {
+  inquirer
+    .prompt([
+      {
+        name: 'addProduct',
+        type: 'input',
+        message: 'What item would you like to add ?'
+      },
+      {
+        name: 'addDepartment',
+        type: 'input',
+        message: 'What department does it belong into?'
+      },
+      {
+        name: 'quantProduct',
+        type: 'input',
+        message: 'How much of the item do you have in stock?',
+        validate: validateUnits
+      },
+      {
+        name: 'priceProduct',
+        type: 'input',
+        message: 'What is the price of this item?',
+        validate: validatePrice
+      }
+    ])
+    .then(function(answer) {
+      var productAdd = answer.addProduct;
+      var departmentAdd = answer.addDepartment;
+      var quantityproduct = answer.quantProduct;
+      var priceproduct = answer.priceProduct;
+
+      connection.query(
+        'INSERT INTO products SET ?',
+        {
+          product_name: productAdd,
+          department_name: departmentAdd,
+          price: priceproduct,
+          stock_quantity: quantityproduct
+        },
+        function(err, data) {
+          if (err) throw err;
+          console.log('\nData Base Updated, new updated list below');
+        }
+      );
+      showProducts();
+      askManager();
+    });
+}
+
+function endConnection() {
+  connection.end();
+}
+
+function validateItem(name) {
+  var reg = /^\d+$/;
+  return reg.test(name) || 'ID should be a number!';
+}
+
+function validatePrice(name) {
+  var reg = /^\d+$/;
+  return reg.test(name) || 'Price should be a number!';
+}
+
+function validateUnits(name) {
+  var reg = /^\d+$/;
+  return reg.test(name) || 'Units should be a number!';
+}
